db5d366361b324b8ed76a5e312e3c1a1c67ed921
Shreyas-Patil-11 <shreyaspatil1311@gmail.com>
2024-08-24 11:31:31 +0530

    mini project

---
+++ Weather/app.js
@@ -0,0 +1,37 @@
+// const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'x-rapidapi-key': 'c7ae26899emshab2a54843284922p1ed1cejsnb03119224713',
+// 		'x-rapidapi-host': 'weatherapi-com.p.rapidapi.com'
+// 	}
+// };
+
+// const url = 'https://weather-api73.p.rapidapi.com/weather?city=delhi';
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'x-rapidapi-key': 'c7ae26899emshab2a54843284922p1ed1cejsnb03119224713',
+// 		'x-rapidapi-host': 'weather-api73.p.rapidapi.com'
+// 	}
+// };
+
+// let url = "https://api.openweathermap.org/data/2.5/onecall?";
+// let lat = "lat=52.229676&";
+// let lon = "lon=21.012229&";
+// let apiOptions = "units=metric&exclude=minutely,alerts&";
+// let apiKey = "appid=dbb76c5d98d5dbafcb94441c6a10236e";
+// let file = queryUrl + lat + lon + apiOptions + apiKey;
+
+// async function getWeather(url, options){
+//     try {
+//         const response = await fetch(url, options);
+//         const result = await response.text();
+//         console.log(result);
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+// getWeather(url,options);
+
